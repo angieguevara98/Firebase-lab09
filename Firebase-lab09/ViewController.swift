54dcf37038b9b6ecb5b4d2c3fc066b8247980eea
Angie Guevara <angie.guevara@tecsup.edu.pe>
2018-10-24 14:38:23 -0500

    Segundo commit

--- Firebase-lab09/ViewController.swift
+++ Firebase-lab09/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInUIDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
     }
 
     override func didReceiveMemoryWarning() {
